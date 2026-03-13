a899157d1416f67b14d748330c726e153a494457
DaLama007 <marwan.lamesch@gmail.com>
2026-03-13 22:41:41 +0100

    Make bot be able to send http request to school webuntis site. From this json extract the text from homeworks so they can be individually sent to the same discord channel as the one the messager used.

--- bot.py
+++ bot.py
@@ -1,15 +1,24 @@
 import discord
 import dotenv as de
 import os
+import requests
+import json
 
 de.load_dotenv()
-
+cookies = {'JSESSIONID':os.getenv('UNTIS_TOKEN')}
 class UntisBot(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-       print(f'Message from {message.author}: {message.author}')
+        channel = message.channel
+        if(message.content == "!hw"):
+            url = os.getenv('SCHOOL_PREFIX') + 'WebUntis/api/homeworks/lessons?startDate=20260301&endDate=20260331'
+            r = requests.get(url,cookies=cookies)
+            data = r.json()
+            homeworks = data['data']['homeworks']
+            for homework in homeworks:
+                await channel.send(homework['text'])
 
 UB = UntisBot()
 UB.run(os.getenv('DISCORD_TOKEN'))
